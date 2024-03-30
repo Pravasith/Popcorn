880a37af5a2bdaef31d9d4c7c5d029ee6fe96901
__prav__ <pravasith@gmail.com>
2024-03-29 21:41:07 -0400

    feat: added windows binaries dir in win64 tc file

--- tc-windows.cmake
+++ tc-windows.cmake
@@ -3,7 +3,9 @@ set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
 
-set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+# where is the target environment located
+set(WINDOWS_BINARIES_DIR ${CMAKE_SOURCE_DIR}/win64)
+set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32 ${WINDOWS_BINARIES_DIR}/glfw3)
 
 # adjust the default behavior of the FIND_XXX() commands: search programs in the
 # host environment
