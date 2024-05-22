d67a827a4ec7b4c4226079d47e8ec0fca78f9546
__prav__ <pravasith@gmail.com>
2024-05-21 21:21:12 -0400

    feat: window library shrink to window

--- include/Popcorn/Events/Event.h
+++ include/Popcorn/Events/Event.h
@@ -11,9 +11,6 @@ enum class Event_T {
   WindowResize,
 
   // Application Events
-  AppTick,
-  AppUpdate,
-  AppRender,
 
   // Keyboard Events
   KeyPressed,
@@ -27,5 +24,18 @@ enum class Event_T {
   MouseScrolled
 };
 
-class Event {};
+class Event {
+public:
+  explicit Event(Event_T eventType) : m_type(eventType){};
+  virtual ~Event() = default;
+
+  [[nodiscard]] virtual Event_T GetEventType() const = 0;
+
+private:
+  Event_T m_type;
+};
+
+class WindowEvent : public Event {
+  /* explicit WindowEvent(); */
+};
 ENGINE_NAMESPACE_END
