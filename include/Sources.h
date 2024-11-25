6bb6a78d025ab7a12fa26a8e74f4c8769e5d5137
__prav__ <pravasith@gmail.com>
2024-11-25 15:32:40 -0500

    chore: src format indentation

--- include/Sources.h
+++ include/Sources.h
@@ -8,11 +8,8 @@ ENGINE_NAMESPACE_BEGIN
 enum ShaderFiles { VkTriVert = 0, VkTriFrag };
 
 static std::unordered_map<ShaderFiles, const char *> PC_src_map = {
-    // Vulkan
     {VkTriVert, "shaders/tri_vert.spv"},
-    // OpenGL
     {VkTriFrag, "shaders/tri_frag.spv"},
-    // ADD MORE
 };
 
 ENGINE_NAMESPACE_END
