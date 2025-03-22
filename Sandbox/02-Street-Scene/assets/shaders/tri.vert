116e8c69ca4c04b5a5a741e30092610fa44e9bfb
__prav__ <pravasith@gmail.com>
2025-03-22 13:34:46 -0400

    feat: scene begin

---
+++ Sandbox/02-Street-Scene/assets/shaders/tri.vert
@@ -0,0 +1,20 @@
+#version 450
+
+layout(set = 0, binding = 0) uniform ProjView {
+    mat4 view;
+    mat4 proj;
+} projViewUbo;
+
+layout(set = 1, binding = 0) uniform ModelView {
+    mat4 model;
+} modelUbo;
+
+layout(location = 0) in vec3 inPosition;
+layout(location = 1) in vec3 inColor;
+layout(location = 0) out vec3 fragColor;
+
+void main() {
+    gl_Position = projViewUbo.proj * projViewUbo.view * modelUbo.model
+        * vec4(inPosition, 1.0);
+    fragColor = inColor;
+}
---
+++ Sandbox/02-Street-Scene/toolchain-win.cmake
@@ -0,0 +1,31 @@
+set(CMAKE_SYSTEM_NAME Windows)
+
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+
+# WINDOWS ENV BINARIES LOCATION
+set(PC_HOME "${CMAKE_SOURCE_DIR}/../../Engine")
+
+# SET CMAKE_FIND ROOT PATHS TO FIND BINARIES OR COMPILERS
+set(CMAKE_FIND_ROOT_PATH
+    # COMPILER
+    /usr/x86_64-w64-mingw32
+    #
+    # POPCORN ENGINE LIB
+    ${PC_HOME}/dist/windows
+    #
+    # THIRD PARTY LIBS
+    "${PC_HOME}/dist/windows/third-party/glfw"
+    "${PC_HOME}/dist/windows/third-party/vulkan-loader"
+    "${PC_HOME}/dist/windows/third-party/vulkan-headers"
+    "${PC_HOME}/dist/windows/third-party/glm"
+    "${PC_HOME}/dist/windows/third-party/vma")
+
+# adjust the default behavior of the FIND_XXX() commands: search programs in the
+# host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# search headers and libraries in the target environment
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
