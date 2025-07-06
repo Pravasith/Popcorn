64328d629ba9ab25e98a7455a4a2ecd8c2cb076f
__prav__ <pravasith@gmail.com>
2025-07-06 16:31:45 -0400

    feat: curves implementation

---
+++ Engine/include/Popcorn/Graphics/Curves.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "GameObject.h"
+#include "GlobalMacros.h"
+#include "Popcorn/Core/Base.h"
+#include <vector>
+
+ENGINE_NAMESPACE_BEGIN
+GFX_NAMESPACE_BEGIN
+
+enum CurveType {
+  Hermite,
+  Bezier,
+};
+
+enum SplineType {
+  CatmullRom,
+  BSpline,
+};
+
+struct AnimationProperty {};
+
+//
+//
+// -------------------------------------------------------------
+// --- CURVE ---------------------------------------------------
+// -------------------------------------------------------------
+
+class Curve {
+public:
+  Curve(float degree = 3.0f)
+      : m_degree(degree) {
+          PC_PRINT("CREATED", TagType::Constr, "Curve")
+        };
+  ~Curve() { PC_PRINT("DESTROYED", TagType::Destr, "Curve") };
+
+  void FollowPath(GameObject &gameObject);
+  void FollowPath(AnimationProperty &animationProp);
+
+  // -----------------------------------------------------------
+  // --- BEZIER ------------------------------------------------
+
+  // -----------------------------------------------------------
+  // --- HERMITE -----------------------------------------------
+
+protected:
+  float m_degree = 1.0f; // linear
+  float m_t = 0.0f;      // 'the' parameter. t as in p(t)
+};
+
+//
+//
+//
+// -------------------------------------------------------------
+// --- SPLINE --------------------------------------------------
+// -------------------------------------------------------------
+
+template <SplineType V> class Spline {
+public:
+  static constexpr SplineType typeValue = V;
+
+public:
+  Spline() { PC_PRINT("CREATED", TagType::Constr, "Spline") };
+  ~Spline() { PC_PRINT("DESTROYED", TagType::Destr, "Spline") };
+
+  void FollowPath(GameObject &gameObject);
+  void FollowPath(AnimationProperty &animationProp);
+
+  // -----------------------------------------------------------
+  // --- BEZIER ------------------------------------------------
+
+  // -----------------------------------------------------------
+  // --- HERMITE -----------------------------------------------
+
+protected:
+  float m_globalT = 0.0f; // 'the' parameter. t as in p(t)
+  std::vector<Curve *> m_knots{};
+};
+
+GFX_NAMESPACE_END
+ENGINE_NAMESPACE_END
