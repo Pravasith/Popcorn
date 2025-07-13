3db65ace6dd0bd1a27e8514d8bbca334f8d4b329
__prav__ <pravasith@gmail.com>
2025-07-12 20:19:02 -0400

    feat: animation classes

--- Engine/include/Popcorn/Graphics/Animation/Animation.h
+++ Engine/include/Popcorn/Graphics/Animation/Animation.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Animation/TimeStations.h"
 #include "GlobalMacros.h"
 #include <initializer_list>
 #include <vector>
@@ -11,16 +12,26 @@ class AnimationTrack {
 public:
   AnimationTrack(const float duration = 500.0f,
                  std::initializer_list<float> timeStations = {})
-      : m_durationMs(duration), m_timeStations(timeStations) {};
+      : m_durationMs(duration), m_timeStations(timeStations) {
+          //
+          // TODO: Create a hashmap for all curve instances, except the hash
+          //       value is custom (hash mixing - paramType value, curveInfo
+          //       values)
+          //
+          // TODO: Asserts for timeStations (sorted)
+          // TODO: Asserts for duration (>0)
+          // TODO: Asserts for timeRails
+          //
+        };
+
   ~AnimationTrack() = default;
 
 public:
-
-
 private:
   float m_elapsedT = 0.0f;
   const float m_durationMs;
   std::vector<float> m_timeStations;
+  std::vector<TimeRail> m_timeRails;
 };
 
 // void AddTimeRails() {
