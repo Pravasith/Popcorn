8d2c661646f3dd74099ec906cba21353c925a961
__prav__ <pravasith@gmail.com>
2025-07-16 15:26:12 -0400

    feat: polycurve splines - (n-1)/n done

--- Engine/include/Popcorn/Graphics/Animation/Splines.h
+++ Engine/include/Popcorn/Graphics/Animation/Splines.h
@@ -3,6 +3,10 @@
 #include "AnimationDefs.h"
 #include "Curves.h"
 #include "GlobalMacros.h"
+#include <algorithm>
+#include <cmath>
+#include <stdexcept>
+#include <utility>
 #include <vector>
 
 ENGINE_NAMESPACE_BEGIN
@@ -10,11 +14,16 @@ GFX_NAMESPACE_BEGIN
 
 template <CurveValueType T> struct SplineSegment {
   Curve<T> *curve;
-  Curve<float> *reparameterizationCurve =
-      nullptr; // t goes through this first, and then the
-               // result value will be used in GetValueAt &
-               // GetDerivativeAt functions of the 'curve'
-  float t0 = 0.0f, t1 = 0.1f;
+
+  // --- Reparameterization ---
+  // t goes through this (reparameterizationCurve) first, and then the result
+  // value will be used in GetValueAt & GetDerivativeAt functions of the 'curve'
+  //
+  // NOTE: just degree 3 for now bc curves are currently designed as cubic, add
+  // quadratic & others later.
+  Curve<float> *reparameterizationCurve = nullptr;
+
+  float t0 = 0.0f, t1 = 1.0f; // segment start & end in terms of t
 };
 
 //
@@ -27,20 +36,80 @@ public:
   Spline() = default;
   virtual ~Spline() = default;
 
-  virtual T GetValueAt_Fast(float t) const = 0;
-  virtual T GetValueAt_Slow(float t) const = 0;
+  virtual T GetValueAt_Fast(float t) const {
+    auto [u, segmentPtr] = GetLocalParameterAndSegment(t);
 
-  virtual T GetFirstDerivativeAt_Fast(float t) const = 0;
-  virtual T GetFirstDerivativeAt_Slow(float t) const = 0;
+    if (segmentPtr->reparameterizationCurve) {
+      u = segmentPtr->reparameterizationCurve->GetValueAt_Fast(u);
+    }
+
+    return segmentPtr->curve->GetValueAt_Fast(u);
+  }
+
+  virtual T GetValueAt_Slow(float t) const {
+    auto [u, segmentPtr] = GetLocalParameterAndSegment(t);
+
+    if (segmentPtr->reparameterizationCurve) {
+      u = segmentPtr->reparameterizationCurve->GetValueAt_Slow(u);
+    }
+
+    return segmentPtr->curve->GetValueAt_Slow(u);
+  }
+
+  virtual T GetFirstDerivativeAt_Fast(float t) const {
+
+    auto [u, segmentPtr] = GetLocalParameterAndSegment(t);
+
+    if (segmentPtr->reparameterizationCurve) {
+      u = segmentPtr->reparameterizationCurve->GetValueAt_Fast(u);
+    }
+
+    return segmentPtr->curve->GetFirstDerivativeAt_Fast(u);
+  }
+  virtual T GetFirstDerivativeAt_Slow(float t) const {
+    auto [u, segmentPtr] = GetLocalParameterAndSegment(t);
+
+    if (segmentPtr->reparameterizationCurve) {
+      u = segmentPtr->reparameterizationCurve->GetValueAt_Slow(u);
+    }
+
+    return segmentPtr->curve->GetFirstDerivativeAt_Slow(u);
+  }
+
+protected:
+  std::pair<float, const SplineSegment<T> *>
+  GetLocalParameterAndSegment(float t) const {
+    t = std::clamp(
+        t, 0.0f, 1.0f); // guarantee t in [0,1], to avoid floating point errors
+
+    assert(0.0f <= t && 1.0f >= t);
+    assert(!m_segments.empty());
+
+    auto it = std::find_if(
+        m_segments.begin(), m_segments.end(), [this, t](auto const &sgmt) {
+          return (sgmt.t0 <= t && t < sgmt.t1) ||
+                 (&sgmt == &this->m_segments.back() && t == 1.0f);
+        });
+
+    if (it == m_segments.end()) {
+      throw std::runtime_error("No segment for t");
+    }
+
+    const SplineSegment<T> &segment = *it;
+
+    assert(segment.t1 - segment.t0 > 0.0f); // avoid div by 0
+
+    float u = (t - segment.t0) /
+              (segment.t1 - segment.t0); // local parameter of the curve
+
+    assert(0.0f <= u && 1.0f >= u);
+
+    return std::make_pair(u, &segment);
+  };
 
 protected:
   std::vector<SplineSegment<T>> m_segments{};
 };
-
-//
-// -------------------------------------------------------------------
-// --- CURVE --------------------------------------------------
-//
 
 GFX_NAMESPACE_END
 ENGINE_NAMESPACE_END
