8a9a6b81cf71060b74cef6d8d694b7d967148ca8
__prav__ <pravasith@gmail.com>
2026-04-10 15:55:27 -0400

    feat: cmake cross compile setup

--- tools/build.sh
+++ tools/build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-mkdir -p build
+mkdir -p build/linux
+mkdir -p build/windows
 
-cmake --preset Popcorn
-cmake -DCMAKE_BUILD_TYPE=DEBUG -S . -B ./build
+read -p "Are you compiling for Windows? Defaults to Linux. y/N" platform
+platform=$(echo "$platform" | tr ':upper:' ':lower:')
+build_dir="./build/linux"
 
-cmake --build build
-cmake --install build
+if [ "$platform" = "y" ]; then
+    cmake --preset PopcornWindowsCross
+    build_dir="./build/windows"
+else
+    cmake --preset PopcornLinux
+fi
+
+cmake -DCMAKE_BUILD_TYPE=DEBUG -S . -B "$build_dir"
+cmake --build "$build_dir"
+cmake --install "$build_dir"
