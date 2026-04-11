b84bb998304b5a8d6c891bba742d0569ac8d6097
__prav__ <pravasith@gmail.com>
2026-04-10 21:37:44 -0400

    feat: CMAKE config for popcorn static library done

--- tools/build.sh
+++ tools/build.sh
@@ -3,17 +3,28 @@
 mkdir -p build/linux
 mkdir -p build/windows
 
-read -p "Are you compiling for Windows? Defaults to Linux. y/N" platform
-platform=$(echo "$platform" | tr ':upper:' ':lower:')
 build_dir="./build/linux"
+build_type="Debug"
 
-if [ "$platform" = "y" ]; then
-    cmake --preset PopcornWindowsCross
-    build_dir="./build/windows"
-else
-    cmake --preset PopcornLinux
+read -p "Are you compiling for Windows? Defaults to Linux. y/N: " \
+    platform
+platform=$(echo "$platform" | tr ':upper:' ':lower:')
+
+read -p "Do you want a Release build? Defaults to Debug. y/N: " \
+    release_build_flag
+release_build_flag=$(echo "$release_build_flag" | tr ':upper:' ':lower:')
+if [ "$release_build_flag" = "y" ]; then
+    build_type="Release"
 fi
 
-cmake -DCMAKE_BUILD_TYPE=DEBUG -S . -B "$build_dir"
+if [ "$platform" = "y" ]; then
+    cmake --preset PopcornWindowsCross -DCMAKE_BUILD_TYPE="$build_type"
+    build_dir="./build/windows"
+else
+    cmake --preset PopcornLinux -DCMAKE_BUILD_TYPE="$build_type"
+fi
+
+
+cmake -S . -B "$build_dir"
 cmake --build "$build_dir"
 cmake --install "$build_dir"
